003db594d9556c7bd8d1b50401fe124fea535b91
Chris McCormick <chris@mccormick.cx>
2021-08-07 21:26:09 +0800

    Log what's happening.

--- 0-download-latest.cljs
+++ 0-download-latest.cljs
@@ -8,8 +8,11 @@
 
 (mkdirp "data")
 
+(print "Starting download.")
+
 (-> (fetch fresh-pins-url)
     (.then #(.json %))
     (.then (fn [json]
-      (fs/writeFileSync "data/latest.json" (js/JSON.stringify json nil 2)))))
+      (fs/writeFileSync "data/latest.json" (js/JSON.stringify json nil 2))
+      (print "Done."))))
 
