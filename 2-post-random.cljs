f240c6fb7ae76d9de03f8723b5c276791e2a1420
Chris McCormick <chris@mccormick.cx>
2021-08-11 22:05:13 +0800

    Fix issues with use of *file*.

--- 2-post-random.cljs
+++ 2-post-random.cljs
@@ -1,7 +1,7 @@
 (ns post
   (:require
     [nbb.core :refer [*file*]]
-    ["path" :as path]
+    ["path" :refer [extname]]
     ["node-fetch" :as fetch]
     ["twitter-api-v2/dist" :refer [TwitterApi]]
     [common :refer [log bail plet env client kv get-pin-image]]))
@@ -10,6 +10,12 @@
               :appSecret (env "TWITTER_API_APP_SECRET")
               :accessToken (env "TWITTER_API_ACCESS_TOKEN")
               :accessSecret (env "TWITTER_API_ACCESS_SECRET")})
+
+(def live (env "LIVE"))
+
+(print live)
+
+(def n *file*)
 
 (doseq [[k v] tw-keys]
   (when (nil? v)
@@ -23,40 +29,40 @@
   (plet [db (client)
          pins (kv "pins")
          posted (kv "posted")
-         _ (log *file* "Running query.")
+         _ (log n "Running query.")
          rows (.query db "select * from keyv where key like 'pins:%' order by random() limit 1")
          pin (first rows)
          pin-hash (second (.split (aget pin "key") ":"))
          pin-data (-> (aget pin "value") (js/JSON.parse) (aget "value") (aget "pin"))
          pin-image (get-pin-image pin-data)
          pin-link (aget pin-data "link")
-         _ (log *file* "First fetch.")
+         _ (log n "First fetch.")
          res (fetch pin-image)
          pin-image (if (aget res "ok")
                      pin-image
                      (.replace pin-image "originals" "564x"))
-         _ (log *file* "Second fetch.")
+         _ (log n "Second fetch.")
          res (fetch pin-image)
          buffer (.buffer res)
-         _ (log *file* "Twitter API.")
+         _ (log n "Twitter API.")
          tw (TwitterApi. (clj->js tw-keys))
          api (aget tw "v1")
-         ext (-> (path/extname pin-image) (.replace "." ""))
+         ext (-> (extname pin-image) (.replace "." ""))
          tweet-text (str "src: " pin-link)
 
-         _ (log *file* "Twitter uploadMedia.")
-         media-id (.uploadMedia api buffer (clj->js {:type ext}))
-         _ (log *file* "Twitter tweet.")
-         tweet (.tweet api tweet-text (clj->js {:media_ids media-id}))
-         _ (log *file* "Update database.")
+         _ (log n "Twitter uploadMedia.")
+         media-id (when live (.uploadMedia api buffer (clj->js {:type ext})))
+         _ (log n "Twitter tweet.")
+         tweet (when live (.tweet api tweet-text (clj->js {:media_ids media-id})))
+         _ (log n "Update database.")
          update-posted (.set posted pin-hash (clj->js {:tweet tweet :pin pin-data :img pin-image :link pin-link}))
          update-pins (.delete pins pin-hash)]
 
-        (log *file* "Posted")
-        (log *file* "Hash:" pin-hash)
-        (log *file* pin-image)
-        (log *file* pin-link)
-        (log *file* (aget res "ok"))
+        (log n "Posted")
+        (log n "Hash:" pin-hash)
+        (log n pin-image)
+        (log n pin-link)
+        (log n (aget res "ok"))
         ;(print "Tweet:" tweet)
         ;(print pin-data)
         (print)))
